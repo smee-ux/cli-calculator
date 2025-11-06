261b63d22be5f53b71230396b93bd21a6d78c0d1
Axel L. <axellindahl@proton.me>
2025-11-06 16:11:59 +0100

    Changelog: 	Added help function, to print a help menu 	Split program into multiple files for readability

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ This is a simply calculator for the command line. A project made for school.
 # To do
 [ ] Add ability to print help page
 [ ] Put funcitons in seperate file
+[ ] Add input check to operand
--- miniProjekt1lommeregner.c
+++ miniProjekt1lommeregner.c
@@ -3,13 +3,7 @@
 //
 
 #include <stdio.h>
-#include <math.h>
-
-void scan_data(char *operatorP, double *operandP);
-
-void do_next_op(char operator, double operand, double *akkumulatorP);
-
-double run_calculator();
+#include "calc.h"
 
 int main(void){
 
@@ -18,101 +12,5 @@ int main(void){
     printf("The final result is %lf\n", run_calculator());
 
     return 0;
-
-}
-
-void scan_data(char *operatorP, double *operandP){
-    
-    int inputCheck = 0;
-    
-    do{
-        printf("Please enter operator> ");
-        scanf(" %c", operatorP);    // Space before %c avoid scanf reading newline from enter
-
-        // If unary operator return
-        if(*operatorP == '#' || *operatorP == '%' || *operatorP == '!' || *operatorP == 'q'){
-            
-            inputCheck = 1;
-            *operandP = 0;
-            return;
-
-        }
-        // If binary continue
-        else if(*operatorP == '+' || *operatorP == '-' || *operatorP == '*' || *operatorP == '/' || *operatorP == '^'){
-            
-            inputCheck = 1;
-            continue;
-
-        }
-        else{
-            printf("Please enter valid operator.\n");
-        }
-    } while(inputCheck == 0);
-
-    // lav input kontrol?
-    printf("Please enter operand> ");
-    scanf("%lf", operandP);
-
-}
-
-void do_next_op(char operator, double operand, double *akkumulatorP){
-    switch(operator){
-        case '+':
-            *akkumulatorP += operand;
-            break;
-        case '-':
-            *akkumulatorP -= operand;
-            break;
-        case '*':
-            *akkumulatorP *= operand;
-            break;
-        case '/':
-            if(operand == 0){
-                break;
-            }
-            *akkumulatorP /= operand;
-            break;
-        case '^':
-            *akkumulatorP = pow(*akkumulatorP, operand);
-            break;
-        case '#':
-            if(*akkumulatorP < 0){
-                break;
-            }
-            *akkumulatorP = sqrt(*akkumulatorP);
-            break;
-        case '%':
-            *akkumulatorP *= -1;
-            break;
-        case '!':
-            if(*akkumulatorP == 0){
-                break;
-            }
-            *akkumulatorP = 1 / *akkumulatorP;
-            break;
-        case 'q':
-            return;
-    }
-}
-
-double run_calculator(){
-
-    char operator;
-    double operand;
-    double akkumulator = 0;
-
-    do{
-        scan_data(&operator, &operand);
-        
-        do_next_op(operator, operand, &akkumulator);
-
-        if(operator != 'q'){
-        printf("Result so far is %lf\n", akkumulator);
-        }
-    
-    }
-    while(operator != 'q');
-
-    return akkumulator;
 
 }
